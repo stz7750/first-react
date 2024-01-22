c30fa2ba93ebf38ad888c720a7fb6a72e8a85b43
stz7750 <rlaxogns7750@gmail.com>
2024-01-22 14:46:45 +0900

    useRef() 컴포넌트 안의 변수 만들기

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 import InputSample from './InputSample';
 import UserList from './UserList';
+import { useRef } from 'react';
 
 function App() {
+  const  nextId = useRef(4);
+  const onCreate = () => {
+    console.log(nextId.current);
+    nextId.current += 1;
+  }
+  
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
   return (
-    <UserList></UserList>
+    <UserList users={users}></UserList>
   );
 }
 
