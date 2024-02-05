2e3237c72fe9cd21b611d9a3ac58627704cdc8d4
stz7750 <rlaxogns7750@gmail.com>
2024-02-05 14:41:35 +0900

    useReducer

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Counter from './Counter';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>
 );
 
